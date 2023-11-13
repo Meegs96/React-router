716d4b9143b60493242e1960ec043fb97539af5d
Meegs96 <miguel.herrera.vindel96@gmail.com>
2023-11-13 03:11:40 -0500

    progress commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
+
 import './App.css'
 import { Routes, Route, Link } from "react-router-dom";
+import Blue from "./components/Blue"
+import Red from "./components/Red"
+import Home from "./components/Home"
 
 function App() {
 
@@ -8,11 +11,16 @@ function App() {
     <>
     <div id="container">
       <h1></h1>
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">
+        <Link to={"/red"}red></Link>
+        <Link to={"blue"}blue></Link>
+        <Link to="/">Home</Link>
+      </div>
       <div id="main-section">
         <Routes>
         <Route path="/blue" element={<h1>Blue</h1>} />
         <Route path="/red" element={<h1>Red</h1>} />
+        <Route path="/" element={<Home/>} />
         </Routes>
       </div>
     </div>
